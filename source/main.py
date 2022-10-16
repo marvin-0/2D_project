5983d7df965b6ea3cc66254ee1a937e87039a844
marvin-0 <alsgh3724@gmail.com>
2022-10-16 20:54:01 +0900

    타이틀 화면 구현

--- source/main.py
+++ source/main.py
@@ -1,9 +1,10 @@
 from pico2d import*
 import character_class
 import play_state
+import title_state
 from monster1_class import *
 import game_framework
 
 open_canvas()
-game_framework.run(play_state)
+game_framework.run(title_state)
 close_canvas()
